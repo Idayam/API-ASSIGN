68a2d8bf9d662d660535c420f655d2ff2bdd1b26
Idayam <mzeeidaya@gmail.com>
2023-05-31 15:54:04 +0300

    Accessories APIs

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-function generateProductCards(data) {
-    const productContainer = document.getElementById("product-box");
-    data.forEach((product) => {
-      const card = document.createElement("div");
-      card.classList.add("product-card");
-      card.innerHTML = `
-              <img src="${product.thumbnail}" alt="${product.name}" />
-              <h2>${product.title}</h2>
-              <div class='productPrice'>
-              <p>${product.price}</p>
-              <button>ADD TO CART</button>
-              </div>
-          `;
-      productContainer.appendChild(card);
-    });
-  }
-  function fetchProducts() {
-    fetch("https://dummyjson.com/products")
-      .then((response) => response.json())
-      .then((data) => {
+function generateProduct(data) {
+  const productContainer = document.getElementById("product-box");
+  data.forEach((product) => {
+    const card = document.createElement("div");
+    card.classList.add("product");
+    card.innerHTML = `
+            <img src="${product.thumbnail}" alt="${product.name}" />
 
-        const returnedData = data.products.slice(0, 7);
-        generateProductCards(returnedData);
-      });
-  }
-  fetchProducts()
+            <div>
+            <p>${product.price}</p>
+            </div>
+        `;
+    productContainer.appendChild(card);
+  });
+}
+function fetchProducts() {
+  fetch("https://dummyjson.com/products")
+    .then((response) => response.json())
+    .then((data) => {
+
+      const returnedData = data.products.slice(0, 7);
+      generateProductCards(returnedData);
+    });
+}
+fetchProducts()
   
   
   
